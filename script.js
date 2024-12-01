edf7596a95b56f4c8648d94c605d4f502debde2e
abdi awale <loyalabdi98@outlook.com>
2024-12-01 20:02:38 +0100

    tog bort extra hänvisning till css

--- script.js
+++ script.js
@@ -20,6 +20,69 @@ dropdownButton.addEventListener('click', function() {
     }, 0); 
   });
 
+  //Objekt för produkter i kategorin senaste
+const products = {
+    //Array med samtliga produkter och dess egenskaper
+    senaste: [
+        { name: "Coca-Cola", price: "20kr", image: "images/cocacola.png", id: "cola" },
+        { name: "Baguette", price: "55kr", image: "images/baguette.jpeg", id: "baguette" },
+        { name: "Chips", price: "20kr", image: "images/chips.png", id: "chips" },
+        { name: "Lokum", price: "35kr", image: "images/lokum.jpeg", id: "lokum" }
+    ],
+    dryck: [ 
+        { name: "Nocco", price: "30kr", image: "images/nocco.png", id: "nocco" },
+        { name: "Coca-Cola", price: "20kr", image: "images/cocacola.png", id: "cola" },
+        { name: "Öl", price: "65kr", image: "images/beer.png", id: "beer" },
+        { name: "Fanta", price: "20kr", image: "images/fanta.png", id: "fanta" }
+    ],
+    snacks: [
+        { name: "Kexchoklad", price: "15kr", image: "images/kexchoklad.png", id: "kex" },
+        { name: "Chips", price: "20kr", image: "images/chips.png", id: "chips" },
+        { name: "Popcorn", price: "15kr", image: "images/popcorn.jpeg", id: "popcorn" },
+        { name: "Godis", price: "20kr", image: "images/godis.jpeg", id: "candy" }
+    ],
+    mellanmal: [
+        { name: "Wasa Knäcke", price: "10kr", image: "images/wasa.png", id: "wasa" },
+        { name: "Lokum", price: "35kr", image: "images/lokum.jpeg", id: "lokum" },
+        { name: "Risifrutti", price: "15kr", image: "images/risifrutti.jpeg", id: "risifrutti" },
+        { name: "Baguette", price: "55kr", image: "images/baguette.jpeg", id: "baguette" }
+    ]
+};
 
+//Funktion för att skriva ut samtliga produkter i htmlen
+function showProducts(category, containerId) {
+    const container = document.getElementById(containerId);
+    //Loopar igenom objektet och hämtar egenskaperna samt lägger till de i html strukturen
+    products[category].forEach((product) => {
+        const productElement = `
+            <div class="produkt d-flex flex-column">
+                <div class="d-flex justify-content-center flex-grow-1 flex-column">
+                    <img src="${product.image}" alt="${product.name}">
+                </div>
+                <h4>${product.name}</h4>
+                <div class="d-flex justify-content-between">
+                    <button class="addera" id="addera-${product.id}">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960">
+                            <path d="M427-428H168v-106h259v-259h106v259h259v106H533v259H427v-259Z" />
+                        </svg>
+                    </button>
+                    <p>${product.price}</p>
+                    <button class="substrahera" id="substrahera-${product.id}">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960">
+                            <path d="M168-428v-106h624v106H168Z" />
+                        </svg>
+                    </button>
+                </div>
+            </div>`;
+        container.innerHTML += productElement;
+    });
+}
+//Inväntar att sidan har laddat innan produkterna visas
+document.addEventListener("DOMContentLoaded", () => {
+    showProducts("senaste", "senaste-container");
+    showProducts("dryck", "dryck-container");
+    showProducts("snacks", "snacks-container");
+    showProducts("mellanmal", "mellanmal-container");
+});
 
 
